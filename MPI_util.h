d7812a3889cd31c4d91857c908466c691ef25f4e
techzbq <zbq_930713@163.com>
2016-12-07 15:01:49 +0800

    update for  make

--- MPI_util.h
+++ MPI_util.h
@@ -8,19 +8,19 @@
 //传输用的tags， 每个tags中包括了传输的数据类型
 class MPI_Tags{
 public:
-    static int MPI_RECV_INT         = 0;
-    static int MPI_RECV_CHAR        = 1;
-    static int MPI_SEND_INT         = 2;
-    static int MPI_SEND_CHAR        = 3;
-    static int MPI_BCAST_INT        = 4;
-    static int MPI_BCAST_CHAR       = 5;
+    const static int MPI_RECV_INT         = 0;
+    const static int MPI_RECV_CHAR        = 1;
+    const static int MPI_SEND_INT         = 2;
+    const static int MPI_SEND_CHAR        = 3;
+    const static int MPI_BCAST_INT        = 4;
+    const static int MPI_BCAST_CHAR       = 5;
 
-    static int MPI_HEART_BEAT       = 10;
-    static int MPI_REGISTEY         = 11;
-    static int MPI_DISCONNECT       = 12;
+    const static int MPI_HEART_BEAT       = 10;
+    const static int MPI_REGISTEY         = 11;
+    const static int MPI_DISCONNECT       = 12;
 
-    static int MPI_BCAST_REQ        = 20; //用于广播询问task剩余情况
-    static int MPI_BCAST_ACK       = 21; //用于回复广播询问task
+    const static int MPI_BCAST_REQ        = 20; //用于广播询问task剩余情况
+    const static int MPI_BCAST_ACK       = 21; //用于回复广播询问task
 };
 
 
