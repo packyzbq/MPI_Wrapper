1ce6612414394f37e0cc0804c2b8b6fbf99faf7b
techzbq <zbq_930713@163.com>
2016-12-07 15:03:52 +0800

    add ServerTest

---
+++ ServerTest.cpp
@@ -0,0 +1,20 @@
+//
+// Created by zhaobq on 2016/12/5.
+//
+
+#include "MPI_Server.h"
+
+
+class test_msg_handler : public Msg_handlerABC{
+public:
+    virtual void recv_commit(int mpi_tags ,void* buffer){
+        cout << "I get a msg, tag=" << mpi_tags <<endl;
+    };
+};
+
+int main(){
+    test_msg_handler* tmh;
+    tmh = new test_msg_handler();
+    MPI_Server server(tmh, "TEST");
+    server.run();
+}
